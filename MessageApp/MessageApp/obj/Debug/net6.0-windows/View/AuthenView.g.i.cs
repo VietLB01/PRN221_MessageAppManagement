ea9c5e7bb5e97879aa42083ba8f3410a6c32b217
Nhat Anh <tanhatanh76@gmail.com>
2023-03-22 22:33:56 +0700

    anhtn

--- MessageApp/MessageApp/obj/Debug/net6.0-windows/View/AuthenView.g.i.cs
+++ MessageApp/MessageApp/obj/Debug/net6.0-windows/View/AuthenView.g.i.cs
@@ -57,13 +57,13 @@ namespace MessageApp.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MessageApp;component/view/authenview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MessageApp;V1.0.0.0;component/view/authenview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\AuthenView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace MessageApp.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
